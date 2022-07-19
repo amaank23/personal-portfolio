fb936e9d6f1b6c9549bec4408c860d54dda0464a
aman-khan-laptop <amaanwaseem24@hotmail.com>
2022-07-20 02:13:28 +0500

    Navbar Done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import './App.css';
+import { About, Footer, Header, Skills, Testimonials, Work } from './container';
+import { Navbar } from './components/';
+
+import './App.scss';
 
 function App() {
-    return <div className="app-container">PAGE</div>;
+    return (
+        <div className="app">
+            <Navbar />
+            <About />
+            <Footer />
+            <Header />
+            <Skills />
+            <Testimonials />
+            <Work />
+        </div>
+    );
 }
 
 export default App;
